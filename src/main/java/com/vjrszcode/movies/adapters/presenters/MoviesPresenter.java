b960e1a12ee4735f1954993e1784723d117e0f39
vjrsz <juniornunes1008@gmail.com>
2023-10-27 13:04:47 -0300

    update

--- src/main/java/com/vjrszcode/movies/adapters/presenters/MoviesPresenter.java
+++ src/main/java/com/vjrszcode/movies/adapters/presenters/MoviesPresenter.java
@@ -2,16 +2,25 @@ package com.vjrszcode.movies.adapters.presenters;
 
 import com.vjrszcode.movies.core.entities.Movie;
 import com.vjrszcode.movies.core.useCases.UseCase;
-import lombok.Builder;
+import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class MoviesPresenter implements UseCase.OutputUseCase {
 
     private final List<MoviePresenter> movies;
 
     public MoviesPresenter(List<MoviePresenter> movies){
         this.movies = movies;
+    }
+
+    public static MoviesPresenter fromListMovies(List<Movie> movies){
+        List<MoviePresenter> listMovies = new ArrayList<>();
+        movies.forEach(movie -> { listMovies.add(new MoviePresenter(movie)); });
+
+        return new MoviesPresenter(listMovies);
     }
 
 }
--- src/main/java/com/vjrszcode/movies/application/services/movie/GetAllMovieService.java
+++ src/main/java/com/vjrszcode/movies/application/services/movie/GetAllMovieService.java
@@ -1,16 +1,11 @@
 package com.vjrszcode.movies.application.services.movie;
 
 import com.vjrszcode.movies.adapters.gateways.MovieGateway;
-import com.vjrszcode.movies.adapters.presenters.MoviePresenter;
 import com.vjrszcode.movies.adapters.presenters.MoviesPresenter;
 import com.vjrszcode.movies.core.useCases.movies.GetAllMovieUseCase;
 import com.vjrszcode.movies.adapters.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Service
 public class GetAllMovieService implements GetAllMovieUseCase {
@@ -23,10 +18,6 @@ public class GetAllMovieService implements GetAllMovieUseCase {
 
     @Override
     public MoviesPresenter execute(MovieGateway input) {
-        List<MoviePresenter> listMovies = new ArrayList<>();
-
-        repository.findAll().forEach(movie -> { listMovies.add(new MoviePresenter(movie)); });
-
-        return new MoviesPresenter(listMovies);
+        return MoviesPresenter.fromListMovies(repository.findAll());
     }
 }
